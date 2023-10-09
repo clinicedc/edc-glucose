e36275364e333560ad4e7fd41ef15a40d4ee6760
erikvw <ew2789@gmail.com>
2023-10-09 16:50:07 -0500

    update paths from edc-visit-schedule, setup

--- edc_glucose/tests/tests/test_glucose_form_validator.py
+++ edc_glucose/tests/tests/test_glucose_form_validator.py
@@ -8,7 +8,7 @@ from edc_lab.constants import EQ
 from edc_reference import site_reference_configs
 from edc_reportable import MILLIMOLES_PER_LITER
 from edc_utils import get_utcnow
-from edc_visit_schedule import site_visit_schedules
+from edc_visit_schedule.site_visit_schedules import site_visit_schedules
 from edc_visit_tracking.constants import SCHEDULED
 from edc_visit_tracking.models import SubjectVisit
 
