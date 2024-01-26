f32110030be7ae9b4dce75cc406ed1fbfe587682
erikvw <ew2789@gmail.com>
2024-01-26 00:37:02 -0600

    fix tests

--- runtests.py
+++ runtests.py
@@ -2,7 +2,6 @@
 import logging
 from pathlib import Path
 
-from edc_constants.constants import IGNORE
 from edc_test_utils import DefaultTestSettings, func_main
 
 app_name = "edc_glucose"
@@ -15,7 +14,7 @@ project_settings = DefaultTestSettings(
     APP_NAME=app_name,
     SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
     LIST_MODEL_APP_LABEL="edc_glucose",
-    EDC_NAVBAR_VERIFY_ON_LOAD=IGNORE,
+    SILENCED_SYSTEM_CHECKS=["sites.E101", "edc_navbar.E002", "edc_navbar.E003"],
     EDC_AUTH_SKIP_SITE_AUTHS=True,
     EDC_AUTH_SKIP_AUTH_UPDATER=True,
     EXTRA_INSTALLED_APPS=[
